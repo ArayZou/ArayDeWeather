ba09cef83180a03616b0b280108700fb9f4f02af
r_zou <r_zou@Ctrip.com>
2015-05-08 15:12:01 +0800

    yo build

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var wrench = require('wrench');
+
+
+var options = {
+  src: 'src',
+  dist: 'dist',
+  tmp: '.tmp',
+  e2e: 'e2e',
+  errorHandler: function(title) {
+    return function(err) {
+      gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+      this.emit('end');
+    };
+  },
+  wiredep: {
+    directory: 'bower_components',
+    exclude: [/foundation\.js/, /foundation\.css/]
+  }
+};
+
+wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function(file) {
+  require('./gulp/' + file)(options);
+});
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+var ghPages = require('gulp-gh-pages');
+var ghPagesOption = {
+  remoteUrl:'git@github.com:ArayZou/ArayDeWeather.git'
+}
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages(ghPagesOption));
+});
---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('arayDeWeather', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'mm.foundation'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
